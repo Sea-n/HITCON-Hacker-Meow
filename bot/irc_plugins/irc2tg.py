33df0a681eaeba24d36c693ede36222367da57dd
秉虎 <s96016641@gmail.com>
2021-05-12 01:56:39 +0800

    irc2tg -> avoid command pass

--- bot/irc_plugins/irc2tg.py
+++ bot/irc_plugins/irc2tg.py
@@ -10,13 +10,15 @@ log: logging.Logger = logging.getLogger(__name__)
 
 
 @event('(?P<event>.*)')  # regex
-def on_all_events(_: IrcBot, event=None):
-    print(event)
+def on_all_events(_: IrcBot, event: str = None):
+    log.info(event)
 
 
 @event('^:(?P<nick>\S+)!\S+@\S+ PRIVMSG (?P<channel>#\S+) :(?P<text>.*)')  # regex
-def on_new_channel_messages(_: IrcBot, nick=None, channel=None, text=None):
-    print(nick, channel, text)
+def on_new_channel_messages(_: IrcBot, nick: str = None, channel: str = None, text: str = None):
+    log.debug(nick, channel, text)
+    if text.startswith("!"):
+        return
     bot.app.loop.create_task(
         bot.app.send_message(os.getenv("TELEGRAM_GROUP"), f"&lt;{html.escape(nick)}&gt;: {html.escape(text)}")
     )
