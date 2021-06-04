195ab3cf65befc04b21d2c371136d783aca85f76
秉虎 <s96016641@gmail.com>
2021-06-04 22:15:04 +0800

    show game when game button is clicked, fix pre_check when inline game had sent

--- bot/plugins/pre_check.py
+++ bot/plugins/pre_check.py
@@ -10,7 +10,7 @@ log: logging.Logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.private, group=-1)
 async def pre_check(_: Client, msg: Message) -> None:
-    if msg.text.startswith("/start"):
+    if msg.text and msg.text.startswith("/start"):
         return
 
     if msg.from_user.id not in get_whitelist():
--- bot/plugins/scanner.py
+++ bot/plugins/scanner.py
@@ -2,7 +2,8 @@ import logging
 from uuid import uuid4
 
 from pyrogram import Client, filters, raw
-from pyrogram.types import InlineQuery, Message
+from pyrogram.raw.base import InputBotInlineResult
+from pyrogram.types import CallbackGame, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InlineQuery, Message
 
 log: logging.Logger = logging.getLogger(__name__)
 GAME_SHORT_NAME: str = "QR_code"
@@ -10,16 +11,25 @@ GAME_SHORT_NAME: str = "QR_code"
 
 @Client.on_inline_query()
 async def inline_query(cli: Client, inline: InlineQuery) -> None:
+    game_keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup([
+        [InlineKeyboardButton(f"Open game {GAME_SHORT_NAME}", callback_game=CallbackGame())],
+        # game button **MUST** in (1, 1)
+    ])
+
+    inline_result: list[InputBotInlineResult] = [
+        raw.types.InputBotInlineResultGame(
+            id=str(uuid4()),
+            short_name=GAME_SHORT_NAME,
+            send_message=raw.types.InputBotInlineMessageGame(reply_markup=await game_keyboard.write(cli))
+        )
+    ]
+
     # equals to
     # await cli.answer_inline_query(inline.id, result, cache_time=0)
     await cli.send(
         raw.functions.messages.SetInlineBotResults(
             query_id=int(inline.id),
-            results=[raw.types.InputBotInlineResultGame(
-                id=str(uuid4()),
-                short_name=GAME_SHORT_NAME,
-                send_message=raw.types.InputBotInlineMessageGame(reply_markup=None)
-            )],
+            results=inline_result,
             cache_time=0,
             gallery=None,
             private=None,
@@ -29,6 +39,13 @@ async def inline_query(cli: Client, inline: InlineQuery) -> None:
     )
 
 
-@Client.on_message(filters.command("send_scanner") & filters.private)
+@Client.on_message(filters.command("game") & filters.private)
 async def send_scanner(cli: Client, msg: Message):
     await cli.send_game(msg.chat.id, GAME_SHORT_NAME)
+
+
+@Client.on_callback_query()
+async def game_button(cli: Client, callback: CallbackQuery):
+    if callback.game_short_name:
+        await callback.answer(url="https://permission.site/")
+        # await callback.answer(url="https://webqr.com/")
