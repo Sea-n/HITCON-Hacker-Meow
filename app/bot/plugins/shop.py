df16f958239886bd94e323573e33fabbabbecfd7
秉虎 <s96016641@gmail.com>
2021-09-26 12:42:01 +0800

    feat: shop get me info

--- app/bot/plugins/shop.py
+++ app/bot/plugins/shop.py
@@ -1,29 +1,49 @@
 import logging
+import os
 
+import requests
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
+
+from models import User, db
 
 log: logging.Logger = logging.getLogger(__name__)
 
 
 @Client.on_message(filters.command("shop") & ~ filters.forwarded)
 async def shop(cli: Client, msg: Message) -> None:
-    keyboard = [[
-        InlineKeyboardButton("T-Shirt", "shop_shirt"),
-        InlineKeyboardButton("電路板", "shop_board"),
-    ], [
-        InlineKeyboardButton("背包", "shop_pack"),
-        InlineKeyboardButton("紀念小物", "shop_others"),
-        InlineKeyboardButton("限定推薦", "shop_recommended"),
-    ], [
-        InlineKeyboardButton("回主選單", "help"),
-    ]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await cli.send_photo(msg.chat.id, "https://i.imgur.com/054ENew.png",
-                         "歡迎來到 *HITCON 線上商城*\n\n"
-                         "使用者：*Hacker Meow* (金級贊助商)\n"
-                         "點數：*4,200 點*\n\n"
-                         "請告訴駭客喵喵你想逛哪個分類", reply_markup=reply_markup)
+    session = db.session()
+    user_orm: User = session.query(User).filter_by(uid=msg.from_user.id).first()
+
+    if user_orm:
+        api_url: str = os.getenv("BASE_URL") + "api/v1/users/me"
+        headers = {"Authorization": f"Bearer {user_orm.jwt_token}"}
+
+        response = requests.get(api_url, headers=headers)
+
+        keyboard = [[
+            InlineKeyboardButton("T-Shirt", "shop_shirt"),
+            InlineKeyboardButton("電路板", "shop_board"),
+        ], [
+            InlineKeyboardButton("背包", "shop_pack"),
+            InlineKeyboardButton("紀念小物", "shop_others"),
+            InlineKeyboardButton("限定推薦", "shop_recommended"),
+        ], [
+            InlineKeyboardButton("回主選單", "help"),
+        ]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await cli.send_photo(msg.chat.id, "https://i.imgur.com/054ENew.png",
+                             f"歡迎來到 **HITCON 線上商城**\n\n"
+                             f"你有 `{response.json().get('points')}` 點\n\n"
+                             f"請告訴駭客喵喵你想逛哪個分類", reply_markup=reply_markup)
+    else:
+        keyboard = [[
+            InlineKeyboardButton("回主選單", "help"),
+        ]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        # TODO: add one page redirect link
+        await cli.send_message(msg.chat.id, "請先進行綁定再進行後續動作", reply_markup=reply_markup)
 
 
 @Client.on_callback_query(filters.regex('^shop'), group=1)
@@ -51,6 +71,7 @@ async def shop_callback(cli: Client, callback: CallbackQuery) -> None:
                                 "請告訴駭客喵喵你想逛哪個分類")
 
     elif callback.data == "shop_shirt":
+        # TODO: iter item from API 3.14
         keyboard = [[
             InlineKeyboardButton("DEFENSE 2019", "shop_shirt_def19"),
             InlineKeyboardButton("CMT 2018", "shop_shirt_cmt18"),
