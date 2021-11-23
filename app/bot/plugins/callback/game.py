15a3ac4bbcf0b4a671fe10026173d85a2e3a2ca7
秉虎 <s96016641@gmail.com>
2021-11-23 22:37:53 +0800

    feat: 修正遊玩方式及點數查詢功能

--- app/bot/plugins/callback/game.py
+++ app/bot/plugins/callback/game.py
@@ -4,7 +4,11 @@ from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMedia, InputMediaPhoto
 
 log: logging.Logger = logging.getLogger(__name__)
-GAME_PHOTO: str = "https://i.imgur.com/Un9ZopT.png"
+GAME_PHOTO: str = "https://imgur.com/a/XhyDmzn"
+INFO: str = """遊戲方式：
+找尋會場中印有駭客貓歷險記的標示，打開 駭客喵喵 Telegram ，輸入題目標號 4 位數後，駭客喵喵將會要求你回答問題，回答成功將可以獲得相對應積分！
+
+快去駭客貓貓放在會場中的 20 題題目標示吧！！據說最快完成全部題目的 20 名會眾將會獲得精美禮品哦！！"""
 
 
 @Client.on_callback_query(filters.regex('^game'))
@@ -13,7 +17,7 @@ async def game_callback(cli: Client, callback: CallbackQuery) -> None:
         InlineKeyboardButton("回到前面", "help"),
     ]]
 
-    media: InputMedia = InputMediaPhoto(GAME_PHOTO, "Caption")
+    media: InputMedia = InputMediaPhoto(GAME_PHOTO, INFO)
 
     await cli.edit_message_media(callback.message.chat.id,
                                  callback.message.message_id,
--- app/bot/plugins/commands/points.py
+++ app/bot/plugins/commands/points.py
@@ -4,6 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from main import bot
+from models import User, db
 
 log: logging.Logger = logging.getLogger(__name__)
 
@@ -12,4 +13,9 @@ log: logging.Logger = logging.getLogger(__name__)
 async def events(_: Client, msg: Message) -> None:
     points: int = bot.get_user_points(msg.from_user.id)
 
-    await msg.reply(f"您目前有 <code>{points}</code> 分")
+    with db.session() as session:
+        user: User = session.query(User).filter_by(uid=msg.from_user.id).first()
+
+        correct_answer_count: str = str(len([_ for _ in user.answered if _.is_passed]))
+        await msg.reply(f"您目前已獲得積分：**{points}** 分\n"
+                        f"已解完題目：**{correct_answer_count}** 題")
