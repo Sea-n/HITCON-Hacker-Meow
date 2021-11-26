fc51aeca101e75b17e95c90b7131b4e0dd24ece0
秉虎 <s96016641@gmail.com>
2021-11-26 11:03:01 +0800

    fix: irc bot -> irc

--- app/bot/plugins/pre/group.py
+++ app/bot/plugins/pre/group.py
@@ -9,8 +9,7 @@ log: logging.Logger = logging.getLogger(__name__)
 @Client.on_message(filters.group, group=-1)
 async def remove_commands(cli: Client, msg: Message) -> None:
     if msg.text:
+        """檢查訊息存在"""
         if msg.text.startswith("/"):
             """清除指令"""
             await cli.delete_messages(msg.chat.id, msg.message_id)
-        """因為 bot 不需要處理群組訊息，中斷後續指令"""
-        msg.stop_propagation()
